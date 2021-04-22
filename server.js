449a56dc40b4cfbc3fe2a8646680a9b278495de8
Marcos J. Peña Pollastri <marcos.p.pollastri@gmail.com>
2021-04-22 17:59:33 -0300

    feat: express and heroku ready

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname+'/www'));
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/www/index.html'));
+});
